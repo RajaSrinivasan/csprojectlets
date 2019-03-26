ed4c9c01f7bf9270cc89feb12d1eb9e5655a2e82
R Srinivasan <rsrinivasan@abiomed.com>
2019-03-26 17:35:08 -0400

    initial checkin

---
+++ hello/hello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace hello
+//{
+    class Program
+    {
+        static void PrintFile(String filename)
+        {
+            Console.WriteLine($"Printing file {filename}");
+        }
+        static void Main(string[] args)
+        {
+            foreach (String arg in args) {
+                PrintFile(arg);
+            }
+        }
+    }
+//}
