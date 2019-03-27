25916fd3e44d2ed88ad15c8e826434d0fc262d09
R Srinivasan <rsrinivasan@abiomed.com>
2019-03-27 18:00:39 -0400

    closer to the requirement

--- hello/hello/Program.cs
+++ hello/hello/Program.cs
@@ -1,22 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-//namespace hello
-//{
-    class Program
+class Program
+{
+    static String name = "hello" ;
+    static String version = "0.0.A" ;
+    static String dashes = "----------------------------";
+    static bool verbose = false;
+    static void PrintFile(String filename)
     {
-        static void PrintFile(String filename)
+        System.IO.StreamReader file = null;
+        try
         {
-            Console.WriteLine($"Printing file {filename}");
+            file = new System.IO.StreamReader(filename);
         }
-        static void Main(string[] args)
+        catch (IOException e)
         {
-            foreach (String arg in args) {
-                PrintFile(arg);
+            Console.WriteLine($"Error opening {filename}");
+            Console.WriteLine(e.Message);
+            return;
+        }
+
+        Console.WriteLine(dashes);
+        Console.WriteLine($"File {filename}");
+
+        if (verbose)
+        {
+            Console.WriteLine(Path.GetFullPath(filename));
+            DateTime creation = File.GetCreationTime(filename);
+            DateTime modified = File.GetLastWriteTime(filename);
+            FileInfo info = new System.IO.FileInfo(filename);
+
+            Console.WriteLine("Created " + creation + "; modified " + modified);
+            Console.WriteLine($"Size {info.Length}");
+            Console.WriteLine(dashes);
+        }
+        int linenum = 0;
+        string line;
+
+        while ((line=file.ReadLine()) != null)
+        {
+            linenum++;
+            String linenumstr = linenum.ToString("D5");
+            //Console.Write(linenum.ToString("D" + (5 - linenumstr.Length).ToString() ) ) ;
+            Console.Write(linenumstr);
+            Console.WriteLine($": {line}");
+        }
+        file.Close();
+    }
+
+    static void ShowHelp()
+    {
+        Console.WriteLine($"{name} - {version}");
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            ShowHelp();
+            return;
+        }
+        foreach (String arg in args)
+        {
+            switch (arg)
+            {
+                case "-h":
+                    ShowHelp();
+                    break;
+                case "-v":
+                    verbose = true;
+                    break;
+                default:
+                    PrintFile(arg);
+                    break;
             }
         }
     }
-//}
+}
+
