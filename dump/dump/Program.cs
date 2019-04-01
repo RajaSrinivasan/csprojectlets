a1db3702314c56bb8c409c2ebb6091887fbc86ec
R Srinivasan <rsrinivasan@abiomed.com>
2019-04-01 17:19:46 -0400

    New utility for hexadecimal dump generation

---
+++ dump/dump/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace dump
+{
+    class Program
+    {
+        static string dashes = "-------------------------------------------------------------";
+        static void DumpFile(string filename, Cli cli)
+        {
+            System.IO.BinaryReader file = null;
+            try
+            {
+                file = new System.IO.BinaryReader(File.Open(filename, FileMode.Open) );
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error opening {filename}");
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.WriteLine(dashes);
+            Console.WriteLine($"File {filename}");
+
+            Console.WriteLine(Path.GetFullPath(filename));
+            DateTime creation = File.GetCreationTime(filename);
+            DateTime modified = File.GetLastWriteTime(filename);
+            FileInfo info = new System.IO.FileInfo(filename);
+
+            Console.WriteLine("Created " + creation + "; modified " + modified);
+            Console.WriteLine($"Size {info.Length}");
+            Console.WriteLine(dashes);
+            byte[] buffer;
+            int offset = 0;
+            while (true)
+            {
+                buffer = file.ReadBytes(cli.blocksize);
+                if (buffer.Length == 0)
+                {
+                    break;
+                }
+                char []bufchars = System.Text.Encoding.ASCII.GetString(buffer).ToCharArray();
+                Console.Write(offset.ToString("D8"));
+                Console.Write(" : ");
+                foreach (char c in bufchars)
+                {
+                    if (Char.IsControl(c))
+                    {
+                        Console.Write('.');
+                    }
+                    else
+                    {
+                        Console.Write(c);
+                    }
+                }
+                if (bufchars.Length < cli.blocksize)
+                {
+                    Console.Write(new string(' ', cli.blocksize - bufchars.Length));
+                }
+                Console.Write(" * ");
+
+                foreach (byte b in buffer)
+                {
+                    Console.Write(b.ToString("X2"));
+                }
+
+                Console.WriteLine();
+
+
+                offset += buffer.Length;
+            }
+
+            file.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            Cli cli = new Cli(args);
+            if (!cli.goodcli)
+            {
+                return;
+            }
+            foreach (string fn in cli.Arguments)
+            {
+                DumpFile(fn,cli);
+            }
+        }
+    }
+}
---
+++ dump/dump/cli.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dump
+{
+    internal class Cli
+    {
+        private const int defaultblocksize = 32;
+        public bool verbose;
+        public bool octal = false;
+        public int blocksize = defaultblocksize;
+        public bool goodcli;
+
+        private List<string> arguments = new List<string>() ;
+
+        public Cli(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                this.ShowHelp();
+                return;
+            }
+
+            foreach (String arg in args)
+            {   bool argisswitch;
+                switch (arg)
+                {
+                    case "-v":
+                    case "--verbose":
+                        this.verbose = true;
+                        break;
+                    case "-h":
+                    case "--help":
+                        this.ShowHelp();
+                        return;
+                    case "-":
+                    case "--octal":
+                        this.octal = true;
+                        break;
+                    default:
+                        argisswitch = false;
+                        if (arg.StartsWith("-b:"))
+                        {
+                            argisswitch = true;
+                            blocksize = int.Parse(arg.Substring(3));
+                        }
+                        if (arg.StartsWith("--blocksize:"))
+                        {
+                            argisswitch = true;
+                            blocksize = int.Parse(arg.Substring(11));
+                        }
+                        if (argisswitch)
+                        {
+                    
+                        }
+                        else
+                        {
+                            if (arg.StartsWith("-"))
+                            {
+                                Console.WriteLine($"Unrecognized switch {arg}");
+                                ShowHelp();
+                                return;
+                            }
+                            this.arguments.Add(arg);
+                        }
+                        break;
+                }
+            }
+            goodcli = true;
+        }
+
+        public List<string> Arguments { get => arguments; set => arguments = value; }
+
+        public void ShowHelp()
+        {
+           Console.WriteLine("-h|--help       - show help");
+           Console.WriteLine("-v|--verbose    - be verbose");
+           Console.WriteLine($"-b|--blocksize  - blocksize. e.g. --blocksize:32 {defaultblocksize}");
+        }
+    }
+   
+}
