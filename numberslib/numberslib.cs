af514e47128bb328cd3eacae5133d2c2d1c14413
Srini <s@srin.me>
2019-04-29 15:03:01 -0400

    restructure into Unary Tests and Core

--- numberslib/numberslib.cs
+++ numberslib/numberslib.cs
@@ -54,5 +54,93 @@ namespace numberslib
             divisors.Sort();
             return divisors.ToArray();
         }
+        static public int[] PrimeFactorsOf(int num)
+        {
+            List<int> factors = new List<int>();
+            //int maxrange = (int)Math.Sqrt((double)num);
+            int maxrange = num / 2;
+            int currnum = num;
+            factors.Add(1);
+            for (int i=2; i <= maxrange; i++)
+            {
+                while ((currnum % i) == 0)
+                {
+                    factors.Add(i);
+                    currnum = currnum / i;
+                }
+            }
+            if (factors.Count == 1)
+            {
+                factors.Add(num);
+            }
+            return factors.ToArray();
+        }
+
+        static public int ProductOf(int[] factors)
+        {
+            int product=1;
+            foreach (int i in factors)
+            {
+                product = product * i;
+            }
+            return product;
+        }
+
+
+        static public int Power(int num, int power)
+        {
+            switch (power)
+            {
+                case 1:
+                    return num;
+                case 2:
+                    return num * num;
+                case 3:
+                    return num * Power(num, 2);
+                case 4:
+                    {
+                        int sq = Power(num, 2);
+                        return sq * sq;
+                    }
+                case 5:
+                    {
+                        int sq = Power(num, 2);
+                        int cube = Power(num, 3);
+                        return sq * cube;
+                    }
+                default:
+                    {
+                        int result = Power(num, 5);
+                        for (int i=0; i<power-5; i++)
+                        {
+                            result = result * num;
+                        }
+                        return result;
+                    }
+            }
+        }
+        static public int[] Power(int[] nums, int power)
+        {
+            int[] powers = new int[nums.Length];
+            for (int i=0; i<nums.Length; i++)
+            {
+                powers[i] = Power(nums[i], power);
+            }
+            return powers;
+        }
+    }
+
+    public class UnaryTests
+    {
+        static public bool IsPrime(int num)
+        {
+            int[] factors = Core.PrimeFactorsOf(num);
+            if (factors.Length == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
     }
 }
--- numberslib/numtest/Program.cs
+++ numberslib/numtest/Program.cs
@@ -5,16 +5,32 @@ namespace numtest
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Tests(int num)
         {
-            Console.WriteLine("No 1056\nDigitsOf");
-            int[] t1 = Core.DigitsOf(1056);
+            Console.WriteLine($"No {num}\nDigitsOf");
+            int[] t1 = Core.DigitsOf(num);
             Core.Show(t1);
             int val = Core.ValueDigitsOf(t1);
             Console.WriteLine($"Value is {val}");
             Console.WriteLine("DivisorsOf");
-            int[] t2 = Core.DivisorsOf(1056);
+            int[] t2 = Core.DivisorsOf(num);
             Core.Show(t2);
+            Console.WriteLine("PrimeFactorsOf");
+            int[] t3 = Core.PrimeFactorsOf(num);
+            Core.Show(t3);
+            int prod = Core.ProductOf(t3);
+            Console.WriteLine($"Product of all those {prod}");
+            bool prime = UnaryTests.IsPrime(num);
+            Console.WriteLine($"IsPrime {prime}");
+        }
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                int argno;
+                argno = int.Parse(arg);
+                Tests(argno);
+            }
 
         }
     }
