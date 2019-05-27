e34d738aea5bf7bdb77fb25ca268b3c18220d7a4
Srini <s@srin.me>
2019-05-27 05:33:05 -0400

    beginnings of CRC projectlet

---
+++ crc/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace crc
+{
+    class Program
+    {
+        static void TestBytes()
+        {
+            CRC crc = new CRC();
+
+            byte b = 0;
+            while (true)
+            {
+                ushort bytecrc;
+                unsafe
+                {
+                    bytecrc = CRC.Checksum(&b, 1);
+                }
+                string bytecrcstr = bytecrc.ToString("X4");
+                Console.WriteLine($"Byte={b} CRC={bytecrcstr}");
+                if (b == 255) break;
+                b++;
+            }
+        }
+
+        static void TestWord()
+        {
+            CRC crc = new CRC();
+
+            ushort w = 0;
+            while (true)
+            {
+                ushort wordcrc;
+                unsafe
+                {
+                    wordcrc = CRC.Checksum(&w, 2);
+                }
+                string wordcrcstr = wordcrc.ToString("X4");
+                string wstr = w.ToString("D6");
+                string whex = w.ToString("X4");
+                if (w == wordcrc)
+                {
+                    Console.WriteLine($"Word={whex} CRC={wordcrcstr}");
+                }
+                if (w == 0xffff) break;
+                w++;
+            }
+        }
+        static void TestTable()
+        {
+            CRC crc = new CRC();
+            //CRC.Generate(0x8408);
+            CRC.Generate(0x1021);   //CCITT
+            //CRC.Generate(0xc0c1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestTable();
+        }
+    }
+}
