27c9ebff4348d578a7ba95777d5ba86fddd52aec
rajasrinivasan <RajaSrinivasan@hotmail.com>
2019-07-14 05:10:16 -0400

    cleanup internal org

--- ipadr/IPAddress.cs
+++ ipadr/IPAddress.cs
@@ -10,8 +10,11 @@ namespace ipadr
     }
     public class IPV4Address : IPAddress
     {
-        private byte[] octets = new byte[4] ;
+
+
         private int subnet_bits;
+
+        private Int32 address;
 
         public IPV4Address()
         {
@@ -19,25 +22,28 @@ namespace ipadr
         }
         public bool Analyze(string adr)
         {
+            byte[] octets = new byte[4];
             string ip = null;
             string snl = null;
             int slashpos = adr.IndexOf('/');
             if (slashpos >= 0)
             {
                 snl = adr.Substring(slashpos + 1);
-                ip = adr.Substring(0, slashpos - 1);
+                ip = adr.Substring(0, slashpos);
                 try
                 {
                     subnet_bits = int.Parse(snl);
                     if ((subnet_bits < 1) || (subnet_bits > 31))
                     {
                         Console.WriteLine("Invalid value for Subnet Mask Length {0,1}", subnet_bits);
+                        return false;
                     }
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
                     Console.WriteLine("Subnet Mask length is invalid {0,1}", snl);
+                    return false;
                 }
             }
             else
@@ -62,10 +68,11 @@ namespace ipadr
                         return false;
                     }
                     octets[ocn] = (byte)ocv;
-
+                    //Console.WriteLine($"Set Octet {ocn} value {ocv}");
                     ip = ip.Substring(dp + 1);
                     if (ocn == 2)
                     {
+                        //Console.WriteLine($"Last octet {ip}");
                         ocv = int.Parse(ip);
                         if ((ocv < 0) || (ocv > 255))
                         {
@@ -73,31 +80,41 @@ namespace ipadr
                             return false;
                         }
                         octets[3] = (byte)ocv;
+                        //Console.WriteLine($"Set Octet 3 value {ocv}");
                     }
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
-                Console.WriteLine("Invalid IP Address");
+                Console.WriteLine("Exception: " + e.Message);
+                Console.WriteLine("Invalid IP Address {0,1}",ip);
                 return false;
             }
-            Console.Write("IP Address {0,1}", ip);
-            Console.Write("Subnet: ");
-            if (snl != null)
-            {
-                Console.WriteLine("Length in bits {0,1}", snl);
-            }
-            else
-            {
-                Console.WriteLine("unknown");
-            }
+
+            Array.Reverse(octets);
+            address = BitConverter.ToInt32(octets);
             return true;
         }
         public void Show()
         {
-            Console.WriteLine("{0,1}.{1,1}.{2,1}.{3,1}", octets[0], octets[1], octets[2], octets[3]);
+            Console.WriteLine("Image {0,1}", Image());
         }
+
+        public string Image()
+        {
+            byte[] adrbytes = BitConverter.GetBytes(address);
+            Array.Reverse(adrbytes);
+            string image = adrbytes[0].ToString() + "."
+                         + adrbytes[1].ToString() + "."
+                         + adrbytes[2].ToString() + "."
+                         + adrbytes[3].ToString();
+            if (subnet_bits > 0)
+            {
+                image = image + "/" + subnet_bits.ToString();
+            }
+            return image;
+        }
+
         public void AnalyzeWithMask(string adr, string mask)
         {
 
